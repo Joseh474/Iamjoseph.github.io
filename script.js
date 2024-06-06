e1eedea50f7d668d38d61ea2f5d599fac9bd2c70
Joseph <94840740+Joseh474@users.noreply.github.com>
2024-06-06 10:01:28 +0300

    script.js
    Signed-off-by: Joseph <94840740+Joseh474@users.noreply.github.com>

--- script.js
+++ script.js
@@ -49,5 +49,17 @@ function toggleContent(link) {
     }
 }
 
+var icon = document.getElementById("icon");
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src="image/MOON.jpeg";
+    } else {
+        icon.src="image/SUN.jpeg";
+    }
+}
+
+
+
     
-    
+     
